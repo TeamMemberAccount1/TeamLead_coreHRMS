d39ed469fe3feb617411675c634dd0f1d351d15b
Suraki <Suraki@SUSH-WORK>
2024-12-11 20:19:03 -0800

    commit to updated the existing test cases

---
+++ src/test/java/com/Hrms/tests/samplejava1.java
@@ -0,0 +1,10 @@
+package com.Hrms.tests;
+
+public class samplejava1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("welcome to git ");
+	}
+
+}
