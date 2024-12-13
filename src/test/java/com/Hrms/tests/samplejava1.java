3bdd401917a775d75ae25b27852e32acde277509
Suraki <Suraki@SUSH-WORK>
2024-12-12 18:30:15 -0800

    worked on the Review comments for that CoreHRMS

--- src/test/java/com/Hrms/tests/samplejava1.java
+++ src/test/java/com/Hrms/tests/samplejava1.java
@@ -5,6 +5,7 @@ public class samplejava1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("welcome to git ");
+System.out.println("Hello");
 	}
 
 }
